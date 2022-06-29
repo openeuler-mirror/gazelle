f02d373216f17fa6c9ca500e49d7f05f876d22a2
杨洋 <yyangoO@outlook.com>
2022-06-29 14:33:23 +0800

    add examples readme, compile components, main file and utilities header

---
+++ examples/main.c
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) 2022-2023. yyangoO.
+* gazelle is licensed under the Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*     http://license.coscl.org.cn/MulanPSL2
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+* PURPOSE.
+* See the Mulan PSL v2 for more details.
+*/
+
+
+#include "utilities.h"
+#include "parameter.h"
+
+
+int32_t main(int argc, char *argv[])
+{
+    int32_t ret = PROGRAM_OK;
+
+    return ret;
+}
