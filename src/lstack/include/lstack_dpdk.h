e55108b4f3ef13556075da292d1dc17a3335fb04
wu-changsheng <wuchangsheng2@huawei.com>
2022-10-08 17:57:40 +0800

    conenct support multi queues

--- src/lstack/include/lstack_dpdk.h
+++ src/lstack/include/lstack_dpdk.h
@@ -51,5 +51,6 @@ int dpdk_ethdev_start(void);
 void dpdk_skip_nic_init(void);
 int32_t dpdk_init_lstack_kni(void);
 void dpdk_restore_pci(void);
+bool port_in_stack_queue(uint32_t src_ip, uint32_t dst_ip, uint16_t src_port, uint16_t dst_port);
 
 #endif /* GAZELLE_DPDK_H */
