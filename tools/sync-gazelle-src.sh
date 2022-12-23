401a52a0f53abc01673bf1ad83af2a7518067840
kircher <majun65@huawei.com>
2022-12-23 16:28:00 +0800

    make tools support sync multi commitid

--- tools/sync-gazelle-src.sh
+++ tools/sync-gazelle-src.sh
@@ -5,7 +5,8 @@
 # How to use it?
 
 # 1. fork gazelle_1 from src-openeuler/gazelle. The name can be specified with FORK_TAR;
-# 2. COMMIT_ID is a committrf hash record from openeuler/gazelle (this repository);
+# 2. COMMIT_ID is a committrf hash record from openeuler/gazelle (this repository). 
+#    Multiple committrf hash records should be separated by spaces;
 # 3. TARGET_BRANCH is a branch from src-openeuler/gazelle;
 # 4. USER/EMAIL will be autimatically obtailed from  the git configuration;
 
@@ -34,52 +35,70 @@ specfile="gazelle.spec"
 echo $COMMIT_ID $TARGET_BRANCH $FORK_TAR $USER $EMAIL
 
 workdir=$(pwd)
-patchname=$(git format-patch -1 $COMMIT_ID | tail -n1)
-gitmsg=$(git log --pretty=format:"%s" -1 $COMMIT_ID | sed -e 's/^![0-9]* //g')
-echo $patchname
-
-cd ..
-if [ ! -d "$FORK_TAR" ];then
-	git clone https://gitee.com/${USER}/${FORK_TAR}.git
+for commitid in ${COMMIT_ID};do
+	cd ${workdir}
+	patchname=$(git format-patch -1 ${commitid} | tail -n1)
 	if [ $? -ne 0 ];then
-		echo "Invail git fork dir!"
-		exit 1;
+		echo "invaild commitid $commitid"
+		exit 1
 	fi
-fi
-cd ${FORK_TAR}
-git clean -dfx
-git checkout .
-git checkout master
-git branch -d $TARGET_BRANCH
-git checkout -b $TARGET_BRANCH origin/$TARGET_BRANCH
-patchnum=$(ls ./*.patch | wc -l)
-let patchnum+=1
-let Patchnum=9000+$patchnum
-patchnum=$(printf "%04d\n" $patchnum)
-new_patchname=$(echo $patchname | sed -e "s/^0001-/${patchnum}-/g")
+	gitmsg=$(git log --pretty=format:"%s" -1 $commitid | sed -e 's/^![0-9]* //g')
+	echo $patchname
 
-echo $new_patchname
+	cd ..
+	if [ ! -d "$FORK_TAR" ];then
+		git clone https://gitee.com/${USER}/${FORK_TAR}.git
+		if [ $? -ne 0 ];then
+			echo "Invail git fork dir!"
+			exit 1;
+		fi
+	fi
 
-#modify release num in spec file
-release=$(grep '^Release:' gazelle.spec | awk '{print $2}')
-release_line=$(grep -n '^Release:' gazelle.spec | cut -f1 -d':')
-let release+=1
+	cd ${FORK_TAR}
+	if [ -z "$done_once" ];then
+		git clean -dfx
+		git checkout .
+		git checkout master
+		git branch -d $TARGET_BRANCH
+		git checkout -b $TARGET_BRANCH origin/$TARGET_BRANCH
+		git pull https://gitee.com/src-openeuler/gazelle.git $TARGET_BRANCH
+	fi
+	patchnum=$(ls ./*.patch | wc -l)
+	let patchnum+=1
+	let Patchnum=9000+$patchnum
+	patchnum=$(printf "%04d\n" $patchnum)
+	new_patchname=$(echo $patchname | sed -e "s/^0001-/${patchnum}-/g")
+
+	echo $new_patchname
+
+	#modify release num in spec file
+	release=$(grep '^Release:' gazelle.spec | awk '{print $2}')
+	release_line=$(grep -n '^Release:' gazelle.spec | cut -f1 -d':')
+	let release+=1
+
+	#add Patch in spec file
+	patch_line=$(grep -n '^Patch' gazelle.spec | tail -n1 | cut -f1 -d':')
+	sed -i "${patch_line} aPatch${Patchnum}:     ${new_patchname}" ${specfile}
+
+	#add changelog in spec file
+	changelog_line=$(grep -n '^%changelog' gazelle.spec | cut -f1 -d':')
+	changelog_data=$(date | awk '{print $1,$2,$3,$6}')
+	changelog_version=$(grep '^Version' gazelle.spec | awk '{print $2}')
+
+	if [ -z "$done_once" ];then
+		sed -i "${changelog_line} G" ${specfile}
+	fi
+	sed -i "${changelog_line} a- ${gitmsg}" ${specfile}
+
+	mv ${workdir}/${patchname} ./${new_patchname}
+
+	if [ -z "$done_once" ];then
+		done_once=1
+	fi
+done
+
 sed -i "${release_line}s/[0-9]*$/${release}/g" ${specfile}
-
-#add Patch in spec file
-patch_line=$(grep -n '^Patch' gazelle.spec | tail -n1 | cut -f1 -d':')
-sed -i "${patch_line} aPatch${Patchnum}:     ${new_patchname}" ${specfile}
-
-#add changelog in spec file
-changelog_line=$(grep -n '^%changelog' gazelle.spec | cut -f1 -d':')
-changelog_data=$(date | awk '{print $1,$2,$3,$6}')
-changelog_version=$(grep '^Version' gazelle.spec | awk '{print $2}')
-
-sed -i "${changelog_line} G" ${specfile}
-sed -i "${changelog_line} a- ${gitmsg}" ${specfile}
 sed -i "${changelog_line} a* ${changelog_data} ${USER} <${EMAIL}> - ${changelog_version}-${release}" ${specfile}
-
-mv ${workdir}/${patchname} ./${new_patchname}
 
 #verify patch apply
 rm -rf ./rpmbuild/SOURCES/
