cbd80d3186c41666ec6ddd7f9aed55a9c8187c2c
Lemmy Huang <huangliming5@huawei.com>
2025-03-21 17:02:04 +0800

    socket: refactor tcp and udp
    Signed-off-by: Lemmy Huang <huangliming5@huawei.com>

--- src/lstack/include/lstack_lockless_queue.h
+++ src/lstack/include/lstack_lockless_queue.h
@@ -15,6 +15,8 @@
 
 #include <stdbool.h>
 
+/* multi-producers single-consumer queue */
+
 typedef struct lockless_queue_node {
     struct lockless_queue_node *volatile next;
 } lockless_queue_node;
@@ -24,6 +26,32 @@ typedef struct lockless_queue {
     lockless_queue_node           *tail __attribute__((__aligned__(64)));
     lockless_queue_node            stub __attribute__((__aligned__(64)));
 } lockless_queue;
+
+
+static inline void lockless_queue_node_set_poped(lockless_queue_node *node)
+{
+    node->next = node;
+}
+
+static inline bool lockless_queue_node_is_poped(lockless_queue_node *node)
+{
+    return node->next == node;
+}
+
+static inline bool lockless_queue_node_test_poped(lockless_queue_node *node)
+{
+    /* 
+     * if (node->next == node) {
+     *     node->next = NULL;
+     *     return 1;
+     * } else {
+     *     return 0;
+     * }
+     */
+    return !!__atomic_compare_exchange_n(
+        (volatile uint64_t *)&node->next, (uint64_t *)&node, (uint64_t)NULL, 
+        false, __ATOMIC_ACQUIRE, __ATOMIC_ACQUIRE);
+}
 
 static inline void lockless_queue_init(lockless_queue *queue)
 {
@@ -57,11 +85,35 @@ static inline int32_t lockless_queue_count(lockless_queue *queue)
     return count;
 }
 
+/*
+ * tail      head
+ *   \_ stub _/
+ * 
+ * tail    old_head    head
+ *   \_ stub _/   node _/
+ * 
+ * tail                head
+ *   \_ stub  ->  node _/
+ */
 static inline void lockless_queue_mpsc_push(lockless_queue *queue, lockless_queue_node *node)
 {
+    lockless_queue_node *old_head;
+
     node->next = NULL;
-    lockless_queue_node *old_head =
-        (lockless_queue_node *)__atomic_exchange_n((void **)&queue->head, (void*)node, __ATOMIC_ACQ_REL);
+
+    old_head = (lockless_queue_node *)__atomic_exchange_n((void **)&queue->head, (void*)node, __ATOMIC_ACQ_REL);
+
+    __atomic_store_n(&old_head->next, node, __ATOMIC_RELEASE);
+}
+
+static inline void lockless_queue_mpsc_test_push(lockless_queue *queue, lockless_queue_node *node)
+{
+    lockless_queue_node *old_head;
+
+    if (!lockless_queue_node_test_poped(node))
+        return;
+
+    old_head = (lockless_queue_node *)__atomic_exchange_n((void **)&queue->head, (void*)node, __ATOMIC_ACQ_REL);
 
     __atomic_store_n(&old_head->next, node, __ATOMIC_RELEASE);
 }
@@ -70,7 +122,13 @@ static inline lockless_queue_node* lockless_queue_mpsc_pop(lockless_queue* queue
 {
     lockless_queue_node *tail = queue->tail;
     lockless_queue_node *next = tail->next;
+    lockless_queue_node *head;
 
+    /*
+     * step1. dequeue stub.
+     * tail                          head
+     *   \_ stub  ->  node  ->  node _/
+     */
     if (tail == &queue->stub) {
         if (next == NULL) {
             return NULL;
@@ -80,21 +138,38 @@ static inline lockless_queue_node* lockless_queue_mpsc_pop(lockless_queue* queue
         next = next->next;
     }
 
+    /*
+     * step2. dequeue tail.
+     * tail      next-\    head
+     *   \_ node  ->  node _/
+     */
     if (next) {
         queue->tail = next;
+        lockless_queue_node_set_poped(tail);
         return tail;
     }
 
-    lockless_queue_node *head = queue->head;
+    /*
+     * step3. enqueue ing.
+     * tail    old_head    head
+     *   \_ node _/   node _/
+     */
+    head = queue->head;
     if (tail != head) {
         return NULL;
     }
 
+    /*
+     * step4. only one node, enqueue stub and dequeue node.
+     * tail      head
+     *   \_ node _/
+     */
     lockless_queue_mpsc_push(queue, &queue->stub);
 
     next = tail->next;
     if (next) {
         queue->tail = next;
+        lockless_queue_node_set_poped(tail);
         return tail;
     }
 
